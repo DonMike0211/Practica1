1e5b568b8b65b5963156707f61c454325015b1cb
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-17 22:45:25 -0600

    Capitulo 31-33

---
+++ 23_03_12_008_Diccionarios.py
@@ -0,0 +1,52 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+consulta = print('El modelo ' + teclado2['Modelo'] + ' cuesta ' + teclado2['Precio'] + '$')
+print(consulta)
+#--------------------------------
+#diccionario 2
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+for x in teclado2:
+	print(x, '=', teclado2[x] + '.')
+#--------------------------------------------------
+#diccionario 3
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+
+del teclado1
+
+del teclado2['Precio']
+del teclado2['Categoría']
+print(teclado2)
+
+
