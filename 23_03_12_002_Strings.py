dc424d20ad73ef8ef1421cc13b7c93111f1278b0
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-17 22:23:04 -0600

    Capitulo 3-6

---
+++ 23_03_12_002_Strings.py
@@ -0,0 +1,46 @@
+mensaje = "Aprendiendo 'Python' ando"
+print(mensaje)
+
+mensaje2 = 'Aprendiendo "python" ando'
+print(mensaje2)
+
+#---------------------------------------
+#String2
+
+concatenar = "Arriba " + "el Atlas."
+
+print(concatenar)
+
+string1 = "Husky"
+string2 = "Siberiano"
+mensaje = string1 + ' ' + string2
+print(mensaje)
+
+palabra = "Husky "
+print(palabra + "Siberiano")
+
+nombre = "Miguel"
+apellido1 = "Ramirez"
+apellido2 = "Santana"
+
+nombreCompleto = nombre + " " + apellido1 + " " + apellido2
+
+print(nombreCompleto)
+
+numero = "20" + "23"
+print(numero)
+#----------------------------------------------------
+#String3
+nombre = "enrique barros fernández".upper()
+print(nombre)
+
+frase = "Esta Es Una Frase Para Ser Formateada.".lower()
+print(frase)
+
+
+frase = "Esta Es Una Frase Para Ser Formateada.".upper()
+print(frase)
+#-------------------------------------------------------
+#String4
+cursos = "Lista de cursos de programación:\n\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL."
+print(cursos)
