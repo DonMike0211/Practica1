3e0360a975336fae361d7d6813f03a8524e923b5
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-17 22:26:52 -0600

    Capitulo 7-9

---
+++ 23_03_12_003_Operadores.py
@@ -0,0 +1,18 @@
+suma = 20 + 23 + 44
+print(suma)
+
+resta = 20 - 23 - 84
+print(resta)
+m
+ultiplicacion = 10 * 87
+print(multiplicacion)
+
+division = 5000 / 230 / 2
+print(division)
+
+operacion = 10 / 5 + 15 - 17
+print(operacion)
+#--------------------------------
+#operadores 2
+elevado = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+print(elevado)
